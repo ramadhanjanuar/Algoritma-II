a509fe9dcdbcb8f7e3bf32c99e13c829292dd97f
mochammadramadhanjanuar <mochammadramadhanjanuarh@gmail.com>
2020-02-11 09:01:29 +0700

    Pertemuan 2: Searching Algorithm

---
+++ Pertemuan - 2/Searching/searching.py
@@ -0,0 +1,25 @@
+def main():
+    numberOfSizeData = int(input())
+    data = [None] * numberOfSizeData
+    data = input().split()
+
+    numberOfSizeDataSearching = int(input()) 
+    dataSearching = [None] * numberOfSizeDataSearching
+    dataSearching = input().split()
+
+    notFound = False
+    for indexDataSearching, valueDataSearching in enumerate(dataSearching):
+        if(notFound and indexDataSearching != 0):
+            print('Tidak ditemukan')
+        for valueData in data:
+            if int(valueDataSearching) == int(valueData):
+                print(f'Ditemukan di index ke {indexDataSearching}')
+                notFound = False
+                break
+            else:
+                notFound = True
+        if(notFound and indexDataSearching == len(dataSearching)-1):
+            print('Tidak ditemukan')
+
+if __name__ == "__main__":
+    main()

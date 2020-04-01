f4e4ba450976edaf406db97f5e8c17860c16e659
mochammadramadhanjanuar <mochammadramadhanjanuarh@gmail.com>
2020-04-02 00:07:31 +0700

    everything

--- Pertemuan - 9/insertion_sort.go
+++ Pertemuan - 9/insertion_sort.go
@@ -3,26 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Selection Sort")
+	fmt.Println("Insertion Sort")
 
 	var arr = []int{46, 52, 21, 22, 11, 13, 60, 80, 100, 1}
 
 	fmt.Println("Before")
 	fmt.Println(arr)
 	fmt.Println("")
-	BubbleSort(&arr)
+	InsertionSort(&arr)
 
 	fmt.Println("After")
 	fmt.Println(arr)
 }
 
-// BubbleSort for bubble sort
-func BubbleSort(a *[]int) {
-	var arr = *a
+// InsertionSort for insertion sort
+func InsertionSort(a *[]int) {
+	var arr []int = *a
 	var n int = len(arr)
 
 	for i := 0; i < n; i++ {
-		for j := n - 1; j > 0; j-- {
+		for j := i; j > 0; j-- {
 			if arr[j] < arr[j-1] {
 				temp := arr[j-1]
 				arr[j-1] = arr[j]
